20cbf5c1d12851c5c83459ce9301cdbb84581b3b
Michael Brown <mjb1431@Michaels-MacBook-Air.local>
2020-08-11 11:46:12 -0500

    Initial commit.

---
+++ tests/TestCase.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Spatie\Honeypot\Tests;
+
+use Carbon\CarbonInterface;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithContainer;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\View;
+use Spatie\Honeypot\HoneypotServiceProvider;
+use Spatie\Honeypot\Tests\TestClasses\FakeEncrypter;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    use InteractsWithContainer;
+
+    protected $testNow = true;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        View::addLocation(__DIR__.'/views');
+
+        if ($this->testNow) {
+            $this->setNow(2019, 1, 1);
+        }
+
+        $this->swap('encrypter', new FakeEncrypter());
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [HoneypotServiceProvider::class];
+    }
+
+    protected function setNow($year, int $month = 1, int $day = 1)
+    {
+        $newNow = $year instanceof CarbonInterface
+            ? $year->copy()
+            : Date::createFromDate($year, $month, $day);
+
+        $newNow = $newNow->startOfDay();
+
+        Date::setTestNow($newNow);
+    }
+}
